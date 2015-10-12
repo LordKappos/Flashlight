31dbf6a03f984a7b01f7874dadf9f8a8e268decf
Scott <lordkappos@gmail.com>
2015-10-12 17:35:47 -0600

    finished

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -9,7 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var onOffButton: UIButton!
+    
+    var isOn: Bool = false
 
+    @IBAction func onOffButtonPressed(sender: UIButton) {
+        if isOn {
+            isOn = false
+            
+            onOffButton.setTitle("On", forState: .Normal)
+            onOffButton.setTitleColor(UIColor .whiteColor(), forState: .Normal)
+            view.backgroundColor = UIColor .blackColor()
+            
+        }else {
+            isOn = true
+            
+            onOffButton.setTitle("Off", forState: .Normal)
+            onOffButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            view.backgroundColor = UIColor.whiteColor()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
